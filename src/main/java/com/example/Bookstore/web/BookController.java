64725b0c763ad3bff7b69d2a4876c18d024a2068
Dmitrii Khlapov <khlapov99@gmail.com>
2021-02-07 19:23:32 +0200

    updated naming (index.html -> booklist.html)

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookController {
     @Autowired
     private BookRepository repository;
-    @RequestMapping(value={"/","/index"})
+    @RequestMapping(value={"/","/booklist"})
     public String bookList(Model model){
         model.addAttribute("books", repository.findAll());
-        return "index";
+        return "booklist";
     }
     @RequestMapping("/add")
     public String addBook(Model model){
@@ -27,12 +27,12 @@ public class BookController {
     @PostMapping("/save")
     public String saveBook(Book book){
         repository.save(book);
-        return "redirect:index";
+        return "redirect:booklist";
     }
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         repository.deleteById(bookId);
-        return "redirect:../index";
+        return "redirect:../booklist";
     }
     @RequestMapping(value = "/edit/{id}")
     public String addStudent(@PathVariable("id") Long bookId, Model model){
